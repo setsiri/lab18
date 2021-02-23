233c16b62a9c09aec4aa4c3acab20aa5b1f23655
setsiri <76035222+setsiri@users.noreply.github.com>
2021-02-23 20:19:31 +0700

    finit

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -1,12 +1,46 @@
 #include<iostream>
+#include<cmath>
+#include<algorithm>
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect *R1,Rect *R2){
+	double p1[2]={R1->x,R1->y};
+	double p2[2]={(R1->x)+(R1->w),R1->y};
+	double p3[2]={R1->x,(R1->y)-(R1->h)};
 
+	double s1[2]={R2->x,R2->y};
+	double s2[2]={(R2->x)+(R2->w),R2->y};
+	double s3[2]={R2->x,(R2->y)-(R2->h)};
 
+	double L1=(max(p1[0],s1[0])-min(p2[0],s2[0]));
+	double aL1=abs(L1);
+	double L2=(min(p1[1],s1[1])-max(p3[1],s3[1]));
+	double aL2=abs(L2);
 
+	cout<<L1<<" "<<L2<<endl;
+	cout<<aL1<<" "<<L2<<endl;
+	
+		if(L1>=0&&L2>=0){
+		return 0;
+	}else{
+		return aL1*aL2;
+	}
+	
+
+}
+
+int main(){
+	Rect R1={1,1,5,5};
+	Rect R2={2,2,5,5};
+	Rect R3 = {1,1,5,5};
+	Rect R4 = {7,2,3,3};
+	Rect R5 = {-1,2,6.9,9.6};
+	Rect R6 = {0,0,1.2,2.5};
+	cout<<overlap(&R1,&R2)<<endl;
+	cout<<overlap(&R3,&R4)<<endl;
+	cout<<overlap(&R5,&R6)<<endl;
 }
--- lab18_3.cpp
+++ lab18_3.cpp
@@ -8,7 +8,10 @@
 using namespace std;
 
 struct student{
-
+	string name;
+	int id;
+	string gender;
+	float gpa;
     //Define struct student with four member (name ,id , gender, gpa);
     
 };
@@ -17,10 +20,10 @@ struct course{
 	string name;
 	int id;
 	vector<string> lecture_list;
-	vector<student *> student_list;
+	vector<student* > student_list;
 };
 
-student * findstudent(vector<student> allstudents,int key){ //There is something wrong in this line.
+student * findstudent(vector<student> &allstudents,int key){ //There is something wrong in this line.
 	for(unsigned int i = 0; i < allstudents.size(); i++){
 		if(allstudents[i].id  == key) return &allstudents[i];
 	}
@@ -62,8 +65,17 @@ int main(){
 	string textline;
 	
 	while(getline(student_file,textline)){
-		student s; 
-		
+		student s;
+		char format[]="%[^,],%d,%[^,],%f";
+		int ID;
+		float GPA;
+		char GENDER[99];
+		char NAME[99];
+		sscanf(textline.c_str(),format,NAME,&ID,GENDER,&GPA);
+		s.id=ID;
+		s.gender=GENDER;
+		s.gpa=GPA;
+		s.name=NAME;
 		//Use sscanf() to split the values in textline and assign those values to the members of struct s;
 
 		allstudents.push_back(s); 		
@@ -84,6 +96,7 @@ int main(){
 				state = 3;
 			}else{
 			
+			allcourses.back().lecture_list.push_back(textline);
 			    //Append (push_back) textline to lecture_list[] of the recently added course in allcourses[];
 			    
 			}			
@@ -93,6 +106,7 @@ int main(){
 			}else{
 				student *p = findstudent(allstudents,atof(textline.c_str()));
 				
+				allcourses.back().student_list.push_back(p);
 				//Append (push_back) p to student_list of the recently added course in allcourses[];
 				
 			}
